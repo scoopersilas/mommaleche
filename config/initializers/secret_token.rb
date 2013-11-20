d42639f08ae6904387af8ae22b89155abba93fe5
Shana Silas <chefshana@me.com>
2013-11-20 00:01:00 -0500

    Inital Commit plus Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mommaleche::Application.config.secret_key_base = '5ad6826c7b79e5fcfc72014da08d0a3fef3d9f23a75036fb621049049aefdff9782054323a718e5978bfc048e28964d8780667570d1428ae18bfc3807a0e04b7'
